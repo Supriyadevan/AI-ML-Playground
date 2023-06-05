0517a4266c4f0922b1e16af23f61bba64b39a399
supriyadevan <Durga@matha123$>
2023-06-05 15:10:22 +0530

    Initial Commit

---
+++ AcademicAce.py
@@ -0,0 +1,16 @@
+def get_max():
+    grades = [9.6, 9.2, 9.7]
+    TopGrade = max(grades)
+    return(TopGrade)
+
+def get_min():
+    grades = [9.6, 9.2, 9.7]
+    LowGrade = min(grades)
+    return(LowGrade)
+
+MaxGrade = get_max()
+print("MaxGrade is: ",MaxGrade)
+
+MinGrade = get_min()
+print("MinGrade is: ",MinGrade)
+
